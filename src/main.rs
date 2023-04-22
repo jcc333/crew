3a6e12590680d832b47cb07bfc73112a648eb4b6
James Clemer <clemer19@gmail.com>
2023-04-22 06:06:34 -0400

    Loads leaf nodes, does not properly nest them

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,11 @@ extern crate humantime;
 extern crate regex;
 
 
-use std::path;
 use std::path::Path;
-use std::ffi::OsStr;
-use std::env;
 use std::fs;
 use log::{trace, debug, info, warn, error};
-use yrs::{Doc, GetString, ReadTxn, StateVector, Text, Transact, Update};
+use yrs::types::ToJson;
+use yrs::{Doc, GetString, ReadTxn, StateVector, Text, Transact, Update, MapPrelim, MapRef, Map};
 use yrs::updates::decoder::Decode;
 use yrs::updates::encoder::Encode;
 use walkdir::{DirEntry, WalkDir};
@@ -65,7 +63,12 @@ fn setup_logger(args: &Arguments) -> Result<(), fern::InitError> {
 
 fn is_included(exclusions: &Vec<String>, entry: &DirEntry) -> bool {
     let path = entry.path();
-    let path = path.canonicalize().unwrap();
+    let canonical_path = path.canonicalize();
+    if canonical_path.is_err() {
+        info!("Could not canonicalize and so skipping: '{}'", path.display());
+        return false;
+    }
+    let path = canonical_path.unwrap();
 
     trace!("Path is: {}", path.display());
     for ancestor in path.ancestors() {
@@ -84,40 +87,51 @@ fn is_included(exclusions: &Vec<String>, entry: &DirEntry) -> bool {
     true
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Arguments::parse();
     setup_logger(&args)?;
 
     let doc_root = Path::new(&args.path).canonicalize()?;
 
     let dir = WalkDir::new(&args.path)
-        .follow_links(true)
+        .follow_links(false)
         .sort_by_file_name()
         .into_iter()
         .filter_entry(|e| is_included(&args.exclude, &e))
         .filter_map(|e| e.ok());
 
-    let mut doc = Doc::new();
+    let doc = Doc::new();
+    let mut root = doc.get_or_insert_map(&args.name);
     for entry in dir {
-        let absolute_path = entry.path().canonicalize()?;
-        let doc_path = absolute_path.strip_prefix(&doc_root)?;
-
-        // add sub-directories as nested maps
-        println!("{} -> {}", doc_path.display(), absolute_path.display());
-        for c in doc_path.components() {
-            println!("{:?}", c);
+        debug!("Processing '{}'", entry.path().display());
+        let acc = &root;
+        //println!("{}, {:?}, is_dir={}", entry.path().display(), entry, entry.path().is_dir());
+        let file_type = entry.file_type();
+        if file_type.is_dir() {
+            //
+        } else if file_type.is_file(){
+            let file_name = entry.file_name().to_str();
+            if let Some(key) = file_name {
+                match std::fs::read_to_string(entry.path()) {
+                    Ok(file_str) => {
+                        trace!("Inserting for '{}', '{}'", key, file_str);
+                        let text = yrs::TextPrelim::new(file_str);
+                        {
+                            let mut txn = acc.transact_mut();
+                            acc.insert(&mut txn, key, text);
+                        }
+                        trace!("Inserted for '{}'", key);
+                    },
+                    Err(error) => error!("Encountered an error reading '{}': '{}'", key, error)
+                }
+            }
         }
     }
-
- /*   let doc = Doc::new();
-    let dir_map = doc.get_or_insert_map(name);
-    for entry in dir {
-        match entry {
-            Ok(entry) => ???,
-            Error(err) => ???,
-        }
-    }
-*/
+    let txn = doc.transact();
+    let json = doc.to_json(&txn);
+    println!("{}", json);
     //let dir = doc.get_or_insert_map(name);
     Ok(())
 }
